45636b35d48275b81c8c1e850031a248e4d9ca57
Rebecca <rebecca@caktusgroup.com>
2015-07-06 13:43:39 -0400

    Django 1.8: Remove deprecated patterns()

--- tracpro/home/urls.py
+++ tracpro/home/urls.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from .views import HomeView
 
-urlpatterns = patterns('', url(r'^$', HomeView.as_view(), name='home.home'))
-urlpatterns += patterns('', url(r'^home/(?P<region>\d+)/$', HomeView.as_view(), name='home.home_for'))
+
+urlpatterns = [
+    url(r'^$', HomeView.as_view(), name='home.home'),
+    url(r'^home/(?P<region>\d+)/$', HomeView.as_view(), name='home.home_for'),
+]
