50b3295b39e685f0826f8de764b264190e976306
Rebecca <rebecca@caktusgroup.com>
2015-08-05 09:38:50 -0400

    Create OrgConfigField abstraction + tests
    settable_cached_property did not work as expected.

--- tracpro/orgs_ext/apps.py
+++ tracpro/orgs_ext/apps.py
@@ -2,21 +2,8 @@ import json
 
 from django.apps import AppConfig
 from django.core.cache import cache
-from django.utils.functional import cached_property
 
-
-class settable_cached_property(cached_property):
-    """
-    Extension of Django's cached_property decorator class that also allows
-    the value of the property to be set.
-    """
-
-    def __init__(self, get_func, set_func, name=None):
-        self.set_func = set_func
-        super(settable_cached_property, self).__init__(get_func, name)
-
-    def __set__(self, instance, value):
-        self.set_func(instance, value)
+from .utils import OrgConfigField
 
 
 class DashOrgConfig(AppConfig):
@@ -33,22 +20,10 @@ class DashOrgConfig(AppConfig):
             if not org.facility_code_field:
                 org.facility_code_field = 'facility_code'
 
-        def _org_get_available_languages(org):
-            return org.get_config('available_languages') or []
-
-        def _org_get_facility_code_field(org):
-            return org.get_config('facility_code_field')
-
         def _org_get_task_result(org, task_type):
             cache_key = Org.LAST_TASK_CACHE_KEY % (org.pk, task_type.name)
             result = cache.get(cache_key)
             return json.loads(result) if result is not None else None
-
-        def _org_set_available_languages(org, value):
-            return org.set_config('available_languages', value, commit=False)
-
-        def _org_set_facility_code_field(org, value):
-            return org.set_config('facility_code_field', value, commit=False)
 
         def _org_set_task_result(org, task_type, result):
             cache_key = Org.LAST_TASK_CACHE_KEY % (org.pk, task_type.name)
@@ -58,20 +33,12 @@ class DashOrgConfig(AppConfig):
         Org.add_to_class('LAST_TASK_CACHE_TTL', 60 * 60 * 24 * 7)  # 1 week
 
         Org.add_to_class('clean', _org_clean)
-        Org.add_to_class('get_available_languages', _org_get_available_languages)
-        Org.add_to_class('get_facility_code_field', _org_get_facility_code_field)
         Org.add_to_class('get_task_result', _org_get_task_result)
-        Org.add_to_class('set_available_languages', _org_set_available_languages)
-        Org.add_to_class('set_facility_code_field', _org_set_facility_code_field)
         Org.add_to_class('set_task_result', _org_set_task_result)
 
         # Never set config directly;
-        # allow config attributes to be set as if they were properties.
-        Org.add_to_class('available_languages', settable_cached_property(
-            _org_get_available_languages,
-            _org_set_available_languages,
-        ))
-        Org.add_to_class('facility_code_field', settable_cached_property(
-            _org_get_facility_code_field,
-            _org_set_facility_code_field,
-        ))
+        # allow config attributes to be set as if they were normal attributes.
+        Org.add_to_class('available_languages',
+                         OrgConfigField("available_languages"))
+        Org.add_to_class('facility_code_field',
+                         OrgConfigField("facility_code_field"))
---
+++ tracpro/orgs_ext/utils.py
@@ -0,0 +1,41 @@
+
+
+class OrgConfigField(object):
+    """
+    Allows setting and retrieving of a config field as if it were a normal
+    class attribute.
+
+    The result of the initial retrieval is cached on the object.
+    This avoids reloading the JSON-encoded config each time that the field is
+    accessed. The retrieval cache is busted when the value is set.
+
+    Usage::
+
+        >>> Org.add_to_class('config_field', OrgConfigField('config_field'))
+        >>> print(Org.config_field)  # calls get_config and caches result
+        None
+        >>> Org.config_field = "hello"  # calls set_config and busts cache
+        >>> print(Org.config_field)  # calls get_config and caches result
+        hello
+        >>> print(Org.config_field)  # uses cached result
+        hello
+
+    """
+
+    def __init__(self, name):
+        # The key used to store this field in the instance's config.
+        self.name = name
+        # Where the cached value of the field is stored on the instance.
+        self.cache_name = "_{}".format(name)
+
+    def __get__(self, instance, owner):
+        if not instance:
+            return self
+        if self.cache_name not in instance.__dict__:
+            val = instance.get_config(self.name)
+            instance.__dict__[self.cache_name] = val
+        return instance.__dict__[self.cache_name]
+
+    def __set__(self, instance, value):
+        instance.set_config(self.name, value, commit=False)
+        instance.__dict__.pop(self.cache_name, None)
