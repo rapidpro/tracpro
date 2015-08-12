34b02c3f12d38cce524cc25fe3c9e7375c4eefe5
Erin Mullaney <emullaney@caktusgroup.com>
2015-08-12 14:38:56 -0400

    Adding form validation to baselineterm form

--- tracpro/baseline/forms.py
+++ tracpro/baseline/forms.py
@@ -18,3 +18,24 @@ class BaselineTermForm(forms.ModelForm):
             'end_date': forms.widgets.DateInput(attrs={'class': 'datepicker'}),
             'org': forms.HiddenInput()
         }
+
+    def clean(self):
+        cleaned_data = super(BaselineTermForm, self).clean()
+
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
+
+        if start_date > end_date:
+            raise forms.ValidationError(
+                    "Start date should be before end date."
+                )
+
+        baseline_question = cleaned_data.get("baseline_question")
+        follow_up_question = cleaned_data.get("follow_up_question")
+
+        if baseline_question.id == follow_up_question.id:
+            raise forms.ValidationError(
+                    "Baseline question and follow up question should be different."
+                )
+
+        return cleaned_data
