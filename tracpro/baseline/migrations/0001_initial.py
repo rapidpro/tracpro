b4491861e7dd779dfa0931c1a4a48a133160580a
Erin Mullaney <emullaney@caktusgroup.com>
2015-08-21 09:38:22 -0400

    Squashin migrations for baselineterm model

--- tracpro/baseline/migrations/0001_initial.py
+++ tracpro/baseline/migrations/0001_initial.py
@@ -10,6 +10,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('polls', '0022_auto_20150716_1722'),
         ('orgs', '0014_auto_20150722_1419'),
+        ('groups', '0005_auto_20150805_2050'),
     ]
 
     operations = [
@@ -17,14 +18,15 @@ class Migration(migrations.Migration):
             name='BaselineTerm',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=255)),
-                ('start_date', models.DateField()),
-                ('end_date', models.DateField()),
+                ('name', models.CharField(help_text='For example: 2015 Term 3 Attendance for P3 Girls', max_length=255)),
+                ('start_date', models.DateTimeField()),
+                ('end_date', models.DateTimeField()),
                 ('baseline_poll', models.ForeignKey(related_name='baseline_terms', to='polls.Poll')),
-                ('baseline_question', smart_selects.db_fields.ChainedForeignKey(auto_choose=True, related_name='baseline_terms', chained_model_field=b'poll', to='polls.Question', chained_field=b'baseline_poll')),
+                ('baseline_question', smart_selects.db_fields.ChainedForeignKey(chained_model_field=b'poll', related_name='baseline_terms', chained_field=b'baseline_poll', auto_choose=True, to='polls.Question', help_text='The most recent response per user will be used as the baseline.')),
                 ('follow_up_poll', models.ForeignKey(to='polls.Poll')),
-                ('follow_up_question', smart_selects.db_fields.ChainedForeignKey(chained_model_field=b'poll', chained_field=b'follow_up_poll', auto_choose=True, to='polls.Question')),
+                ('follow_up_question', smart_selects.db_fields.ChainedForeignKey(chained_model_field=b'poll', chained_field=b'follow_up_poll', auto_choose=True, to='polls.Question', help_text='Responses over time to compare to the baseline.')),
                 ('org', models.ForeignKey(related_name='baseline_terms', verbose_name='Organization', to='orgs.Org')),
+                ('region', models.ForeignKey(related_name='baseline_terms', to='groups.Region')),
             ],
         ),
     ]
