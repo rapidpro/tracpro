dd6172b2f7823ca54296eb603116b36fbe1dc61e
Erin Mullaney <emullaney@caktusgroup.com>
2015-08-24 14:19:42 -0400

    JS: Modifying change() call and pointing to the closest form-group element for show/hide()

--- tracpro/static/js/baseline.js
+++ tracpro/static/js/baseline.js
@@ -3,27 +3,27 @@ $(function() {
 
     // Display/hide question if poll is selected
     if (!$("#id_baseline_poll").val()) {
-        $("#id_baseline_question").parent().parent().hide();
+        $("#id_baseline_question").closest(".form-group").hide();
     }
-    $("#id_baseline_poll").on("change", function() {
-      if(this.value) {
-        $("#id_baseline_question").parent().parent().show();
+    $("#id_baseline_poll").change(function() {
+      if($(this).val()) {
+        $("#id_baseline_question").closest(".form-group").show();
       }
       else {
-        $("#id_baseline_question").parent().parent().hide();
+        $("#id_baseline_question").closest(".form-group").hide();
       }
     });
 
     // Display/hide question if poll is selected
     if (!$("#id_follow_up_poll").val()) {
-        $("#id_follow_up_question").parent().parent().hide();
+        $("#id_follow_up_question").closest(".form-group").hide();
     }
-    $("#id_follow_up_poll").on("change", function() {
-      if(this.value) {
-        $("#id_follow_up_question").parent().parent().show();
+    $("#id_follow_up_poll").change(function() {
+      if($(this).val()) {
+        $("#id_follow_up_question").closest(".form-group").show();
       }
       else {
-        $("#id_follow_up_question").parent().parent().hide();
+        $("#id_follow_up_question").closest(".form-group").hide();
       }
     });
 });
