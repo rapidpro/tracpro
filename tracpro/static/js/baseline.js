53feb30baebd84aaf5292bf6e2a65c962b675950
Erin Mullaney <emullaney@caktusgroup.com>
2015-08-24 14:24:18 -0400

    Calling the change() function to show/hide on page load

--- tracpro/static/js/baseline.js
+++ tracpro/static/js/baseline.js
@@ -1,10 +1,6 @@
 $(function() {
     $("input[class^='datepicker']").datepicker();
 
-    // Display/hide question if poll is selected
-    if (!$("#id_baseline_poll").val()) {
-        $("#id_baseline_question").closest(".form-group").hide();
-    }
     $("#id_baseline_poll").change(function() {
       if($(this).val()) {
         $("#id_baseline_question").closest(".form-group").show();
@@ -14,10 +10,6 @@ $(function() {
       }
     });
 
-    // Display/hide question if poll is selected
-    if (!$("#id_follow_up_poll").val()) {
-        $("#id_follow_up_question").closest(".form-group").hide();
-    }
     $("#id_follow_up_poll").change(function() {
       if($(this).val()) {
         $("#id_follow_up_question").closest(".form-group").show();
@@ -26,4 +18,6 @@ $(function() {
         $("#id_follow_up_question").closest(".form-group").hide();
       }
     });
+
+    $("#id_follow_up_poll,#id_baseline_poll").change();
 });
