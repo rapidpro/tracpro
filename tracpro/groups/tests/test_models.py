1d08128663319a655f7d09fdb93f3f48a9539234
Rebecca <rebecca@caktusgroup.com>
2015-09-01 20:51:04 -0400

    Tests for region deactivation & sync

--- tracpro/groups/tests/test_models.py
+++ tracpro/groups/tests/test_models.py
@@ -2,114 +2,208 @@ import mock
 
 from django.contrib.auth.models import User
 from django.test.utils import override_settings
-from django.utils import timezone
 
-from temba.types import Contact as TembaContact, Group as TembaGroup
+from temba.types import Group as TembaGroup
 
-from tracpro.contacts.models import Contact
-from tracpro.test.cases import TracProDataTest
+from tracpro.test import factories
+from tracpro.test.cases import TracProDataTest, TracProTest
 
 from .. import models
 
 
-class TestRegion(TracProDataTest):
+@override_settings(
+    CELERY_ALWAYS_EAGER=True,
+    CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
+    BROKER_BACKEND='memory',
+)
+class TestRegion(TracProTest):
+
+    def setUp(self):
+        super(TracProTest, self).setUp()
+        self.org = factories.Org()
+
+        self.temba_groups = {
+            '1': TembaGroup.create(uuid="1", name="Uganda"),
+            '2': TembaGroup.create(uuid="2", name="Kampala"),
+            '3': TembaGroup.create(uuid="3", name="Entebbe"),
+            '4': TembaGroup.create(uuid="4", name="Makerere"),
+        }
+
+        self.uganda = factories.Region(
+            org=self.org, uuid='1', name="Uganda")
+        self.kampala = factories.Region(
+            org=self.org, uuid='2', name="Kampala", parent=self.uganda)
+        self.entebbe = factories.Region(
+            org=self.org, uuid='3', name="Entebbe", parent=self.uganda)
+        self.makerere = factories.Region(
+            org=self.org, uuid='4', name="makerere", parent=self.kampala)
+        self.inactive = factories.Region(
+            org=self.org, uuid='5', name="inactive", parent=self.kampala,
+            is_active=False)
+
+    def refresh_regions(self):
+        """Refresh all regions from the database."""
+        for region in (self.uganda, self.kampala, self.entebbe, self.makerere,
+                       self.inactive):
+            region = region.refresh_from_db()
 
     def test_create(self):
-        zabul = models.Region.create(self.unicef, "Zabul", 'G-101')
-        jan = self.create_contact(
-            self.unicef, "Jan", 'tel:1234', zabul, self.group1, 'C-101')
-        bob = User.create(
-            self.unicef, "Bob", "bob@unicef.org", "pass", False, [zabul])
+        """The create() method is a shortcut for creating a new Region."""
+        region = models.Region.create(self.org, "Zabul", 'G-101')
+        contact = factories.Contact(org=self.org, region=region)
+        user = User.create(
+            self.org, "User", "user@unicef.org", "pass", False, [region])
 
-        self.assertEqual(zabul.org, self.unicef)
-        self.assertEqual(zabul.name, "Zabul")
-        self.assertEqual(zabul.uuid, 'G-101')
-        self.assertEqual(list(zabul.get_contacts()), [jan])
-        self.assertEqual(list(zabul.get_users()), [bob])
+        self.assertEqual(region.org, self.org)
+        self.assertEqual(region.name, "Zabul")
+        self.assertEqual(region.uuid, 'G-101')
+        self.assertEqual(list(region.get_contacts()), [contact])
+        self.assertEqual(list(region.get_users()), [user])
 
     def test_get_all(self):
-        self.assertEqual(len(models.Region.get_all(self.unicef)), 3)
-        self.assertEqual(len(models.Region.get_all(self.nyaruka)), 1)
+        """Return all active regions for the organization."""
+        self.assertEqual(set(models.Region.get_all(self.org)), set([
+            self.uganda,
+            self.kampala,
+            self.entebbe,
+            self.makerere,
+        ]))
 
-    @override_settings(
-        CELERY_ALWAYS_EAGER=True,
-        CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
-        BROKER_BACKEND='memory',
-    )
+    def test_deactivate_no_children(self):
+        """Deactivation workflow when region has no children."""
+        self.makerere.deactivate()
+        self.refresh_regions()
+
+        self.assertFalse(self.makerere.is_active)
+        self.assertIsNone(self.makerere.parent)
+        self.assertEqual(list(self.makerere.children.all()), [])
+
+    def test_deactivate_with_children(self):
+        """Deactivation workflow when region has children."""
+        self.kampala.deactivate()
+        self.refresh_regions()
+
+        self.assertFalse(self.kampala.is_active)
+        self.assertIsNone(self.kampala.parent)
+        self.assertEqual(list(self.kampala.children.all()), [])
+        self.assertEqual(self.makerere.parent, self.uganda)
+        self.assertEqual(self.inactive.parent, self.uganda)
+
+    def test_deactivate_top_level(self):
+        """Deactivation workflow for top-level region."""
+        self.uganda.deactivate()
+        self.refresh_regions()
+
+        self.assertFalse(self.uganda.is_active)
+        self.assertIsNone(self.uganda.parent)
+        self.assertEqual(list(self.uganda.children.all()), [])
+        self.assertIsNone(self.kampala.parent)
+        self.assertIsNone(self.entebbe.parent)
+        self.assertEqual(self.makerere.parent, self.kampala)
+        self.assertEqual(self.inactive.parent, self.kampala)
+
     @mock.patch('dash.orgs.models.TembaClient.get_groups')
     @mock.patch('dash.orgs.models.TembaClient.get_contacts')
-    def test_sync_with_groups(self, mock_get_contacts, mock_get_groups):
-        mock_get_groups.return_value = [
-            TembaGroup.create(
-                uuid='G-101',
-                name="New region",
-                size=2,
-            ),
-            TembaGroup.create(
-                uuid='G-102',
-                name="Other region",
-                size=1,
-            ),
-        ]
-        mock_get_contacts.return_value = [
-            TembaContact.create(
-                uuid='C-101',
-                name="Jan",
-                urns=['tel:123'],
-                groups=['G-101', 'G-005'],
-                fields={
-                    'chat_name': "jan",
-                    'language': 'eng',
-                    'modified_on': timezone.now(),
-                },
-            ),
-            TembaContact.create(
-                uuid='C-102',
-                name="Ken",
-                urns=['tel:234'],
-                groups=['G-101', 'G-006'],
-                fields={
-                    'chat_name': "ken",
-                    'language': 'eng',
-                    'modified_on': timezone.now(),
-                }
-            ),
-        ]
+    def test_sync_deactivate(self, mock_get_contacts, mock_get_groups):
+        """Deactivate any group whose UUID is not given."""
+        mock_get_groups.return_value = self.temba_groups.values()
+        uuids = ['1', '2', '4']  # no Entebbe
+        models.Region.sync_with_temba(self.org, uuids)
+        self.refresh_regions()
 
-        # select one new group
-        models.Region.sync_with_groups(self.unicef, ['G-101'])
-        self.assertEqual(self.unicef.regions.filter(is_active=True).count(), 1)
-        # existing de-activated
-        self.assertEqual(self.unicef.regions.filter(is_active=False).count(), 3)
+        self.assertEqual(set(models.Region.get_all(self.org)), set([
+            self.uganda,
+            self.kampala,
+            self.makerere,
+        ]))
+        self.assertEqual(mock_get_groups.call_count, 1)
+        self.assertEqual(mock_get_contacts.call_count, 1)
 
-        new_region = models.Region.objects.get(uuid='G-101')
-        self.assertEqual(new_region.name, "New region")
-        self.assertTrue(new_region.is_active)
+    @mock.patch('dash.orgs.models.TembaClient.get_groups')
+    @mock.patch('dash.orgs.models.TembaClient.get_contacts')
+    def test_sync_update_existing(self, mock_get_contacts, mock_get_groups):
+        """Update existing group with new information from remote."""
+        self.temba_groups['2'].name = "Changed"  # Kampala
+        mock_get_groups.return_value = self.temba_groups.values()
+        uuids = ['1', '2', '3', '4']
+        models.Region.sync_with_temba(self.org, uuids)
+        self.refresh_regions()
 
-        # check contact changes
-        self.assertEqual(self.unicef.contacts.filter(is_active=True).count(), 2)
-        # existing de-activated
-        self.assertEqual(self.unicef.contacts.filter(is_active=False).count(), 5)
+        self.assertEqual(set(models.Region.get_all(self.org)), set([
+            self.uganda,
+            self.kampala,
+            self.makerere,
+            self.entebbe,
+        ]))
+        self.assertEqual(self.kampala.name, "Changed")
+        self.assertEqual(self.kampala.parent, self.uganda)
+        self.assertEqual(mock_get_groups.call_count, 1)
+        self.assertEqual(mock_get_contacts.call_count, 1)
 
-        jan = Contact.objects.get(uuid='C-101')
-        self.assertEqual(jan.name, "Jan")
-        self.assertEqual(jan.urn, 'tel:123')
-        self.assertEqual(jan.region, new_region)
-        self.assertTrue(jan.is_active)
+    @mock.patch('dash.orgs.models.TembaClient.get_groups')
+    @mock.patch('dash.orgs.models.TembaClient.get_contacts')
+    def test_sync_reactivate(self, mock_get_contacts, mock_get_groups):
+        """Reactivate a group that existed previously."""
+        mock_get_groups.return_value = self.temba_groups.values()
+        self.entebbe.deactivate()
+        uuids = ['1', '2', '3', '4']
+        models.Region.sync_with_temba(self.org, uuids)
+        self.refresh_regions()
 
-        # change group and contacts on chatpro side
-        models.Region.objects.filter(name="New region").update(name="Huh?", is_active=False)
-        jan.name = "Janet"
-        jan.save()
-        Contact.objects.filter(name="Ken").update(is_active=False)
+        self.assertEqual(set(models.Region.get_all(self.org)), set([
+            self.uganda,
+            self.kampala,
+            self.makerere,
+            self.entebbe,
+        ]))
+        self.assertTrue(self.entebbe.is_active)
+        self.assertEqual(self.entebbe.name, "Entebbe")
+        self.assertEqual(self.entebbe.parent, None)
+        self.assertEqual(mock_get_groups.call_count, 1)
+        self.assertEqual(mock_get_contacts.call_count, 1)
 
-        # re-select new group
-        models.Region.sync_with_groups(self.unicef, ['G-101'])
+    @mock.patch('dash.orgs.models.TembaClient.get_groups')
+    @mock.patch('dash.orgs.models.TembaClient.get_contacts')
+    def test_sync_create_new(self, mock_get_contacts, mock_get_groups):
+        """Create a new group if the UUID hasn't been seen before."""
+        self.temba_groups['6'] = TembaGroup.create(uuid='6', name="New")
+        mock_get_groups.return_value = self.temba_groups.values()
+        uuids = ['1', '2', '3', '4', '6']
+        models.Region.sync_with_temba(self.org, uuids)
+        self.refresh_regions()
 
-        # local changes should be overwritten
-        self.assertEqual(self.unicef.regions.get(is_active=True).name, 'New region')
-        self.assertEqual(self.unicef.contacts.filter(is_active=True).count(), 2)
-        Contact.objects.get(name="Jan", is_active=True)
+        new_region = models.Region.objects.get(uuid='6')
+        self.assertEqual(set(models.Region.get_all(self.org)), set([
+            self.uganda,
+            self.kampala,
+            self.makerere,
+            self.entebbe,
+            new_region,
+        ]))
+        self.assertEqual(new_region.name, "New")
+        self.assertIsNone(new_region.parent, None)
+        self.assertEqual(mock_get_groups.call_count, 1)
+        self.assertEqual(mock_get_contacts.call_count, 1)
+
+    @mock.patch('dash.orgs.models.TembaClient.get_groups')
+    @mock.patch('dash.orgs.models.TembaClient.get_contacts')
+    def test_sync_removed_remote(self, mock_get_contacts, mock_get_groups):
+        """Deactivate a group locally if it has been removed from the remote."""
+        self.temba_groups.pop('3')  # Entebbe
+        mock_get_groups.return_value = self.temba_groups.values()
+        uuids = ['1', '2', '3', '4']
+        models.Region.sync_with_temba(self.org, uuids)
+        self.refresh_regions()
+
+        self.assertFalse(self.entebbe.is_active)
+        self.assertIsNone(self.entebbe.parent)
+        self.assertEqual(set(models.Region.get_all(self.org)), set([
+            self.uganda,
+            self.kampala,
+            self.makerere,
+        ]))
+        self.assertEqual(mock_get_groups.call_count, 1)
+        self.assertEqual(mock_get_contacts.call_count, 1)
 
 
 class TestGroup(TracProDataTest):
