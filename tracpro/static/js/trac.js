8da6cbe2755df887712680356a511658b3cf6ad4
Rebecca <rebecca@caktusgroup.com>
2015-09-04 12:10:40 -0400

    Display region drop-down as a hierarchy

---
+++ tracpro/static/js/trac.js
@@ -0,0 +1,20 @@
+$(function() {
+  /* Set indentation in sub-menus.
+   *
+   * Using the indenter allows every menu item (regardless of depth)
+   * to be clickable over the full width of the menu.
+   */
+  $(".submenu").each(function(i, submenu) {
+    $(submenu).find(">li").each(function(i, li) {
+      // Progressively increase indentation with each pass over the menu item.
+      var indenter = $(li).find(".indenter");
+      indenter.css("width", parseInt(indenter.css("width")) + 20 + "px");
+    });
+  });
+
+  $(".toggle-subregions").click(function(e) {
+    debugger;
+    $("#toggle-subregions-form").submit();
+    return false;
+  });
+});
