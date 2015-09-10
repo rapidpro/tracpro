80d335c91f2059a64509581ead59fd1dc82faefe
Erin Mullaney <emullaney@caktusgroup.com>
2015-09-10 16:31:23 -0400

    WIP

--- tracpro/home/views.py
+++ tracpro/home/views.py
@@ -9,7 +9,7 @@ from smartmin.users.views import SmartTemplateView
 from tracpro.polls.models import Poll
 
 
-class HomeView(OrgPermsMixin, SmartTemplateView):
+class HomeView(OrgPermsMixin, SmartTemplateView, BaselineTermCRUDL):
     """TracPro homepage"""
 
     title = _("Home")
@@ -21,4 +21,22 @@ class HomeView(OrgPermsMixin, SmartTemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['polls'] = Poll.get_all(self.request.org).order_by('name')
+
+
+        """
+        # Loop through all baseline terms, until we find one with data
+        for baselineterm in BaselineTerm.objects.all().order_by('-end_date'):
+            baseline_dict, baseline_dates = baselineterm.get_baseline(region=None)
+            if baseline_dict:
+                follow_ups, dates = baselineterm.get_follow_up(region=None)
+                # Create a list of all dates for this poll
+                # Example: date_list =  ['09/01', '09/02', '09/03', ...]
+                date_list = []
+                for date in dates:
+                    date_formatted = date.strftime('%m/%d')
+                    date_list.append(date_formatted)
+                context['date_list'] = date_list
+
+        """
+
         return context
