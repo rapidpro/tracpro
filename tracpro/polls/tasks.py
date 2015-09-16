ec7b5723e353c669e82c47aae6b057d65f7b6b2c
Erin Mullaney <emullaney@caktusgroup.com>
2015-09-16 14:33:36 -0400

    Adding exclude to filter that gets the newest Response time, exclude out spoof data. Reordering import statements. Rename r variable to longer less generic name.

--- tracpro/polls/tasks.py
+++ tracpro/polls/tasks.py
@@ -1,17 +1,15 @@
 from __future__ import absolute_import, unicode_literals
 
+from django.utils import timezone
+
 from celery.utils.log import get_task_logger
-
-from dash.orgs.models import Org
-from dash.utils import datetime_to_ms
-
 from djcelery_transactions import task
-
 from django_redis import get_redis_connection
 
 from temba.utils import parse_iso8601, format_iso8601
 
-from django.utils import timezone
+from dash.orgs.models import Org
+from dash.utils import datetime_to_ms
 
 from tracpro.contacts.models import Contact
 
@@ -26,11 +24,11 @@ LAST_FETCHED_RUN_TIME_KEY = 'org:%d:last_fetched_run_time'
 @task
 def fetch_all_runs():
     """Fetches flow runs for all orgs."""
-    r = get_redis_connection()
+    redis_connection = get_redis_connection()
 
     # only do this if we aren't already running so we don't get backed up
-    if not r.get(FETCH_ALL_RUNS_LOCK):
-        with r.lock(FETCH_ALL_RUNS_LOCK, timeout=600):
+    if not redis_connection.get(FETCH_ALL_RUNS_LOCK):
+        with redis_connection.lock(FETCH_ALL_RUNS_LOCK, timeout=600):
             logger.info("Starting flow run fetch for all orgs...")
 
             for org in Org.objects.filter(is_active=True).prefetch_related('polls'):
@@ -45,17 +43,19 @@ def fetch_org_runs(org):
     poll responses.
     """
     from tracpro.orgs_ext.constants import TaskType
-    from tracpro.polls.models import Poll, Response
+    from tracpro.polls.models import Poll, PollRun, Response
 
     client = org.get_temba_client()
-    r = get_redis_connection()
+    redis_connection = get_redis_connection()
     last_time_key = LAST_FETCHED_RUN_TIME_KEY % org.pk
-    last_time = r.get(last_time_key)
+    last_time = redis_connection.get(last_time_key)
 
     if last_time is not None:
         last_time = parse_iso8601(last_time)
     else:
-        newest_run = Response.objects.filter(pollrun__poll__org=org).order_by('-created_on').first()
+        newest_runs = Response.objects.filter(pollrun__poll__org=org).order_by('-created_on')
+        newest_runs = newest_runs.exclude(pollrun__pollrun_type=PollRun.TYPE_SPOOFED)
+        newest_run = newest_runs.first()
         last_time = newest_run.created_on if newest_run else None
 
     until = timezone.now()
@@ -79,7 +79,7 @@ def fetch_org_runs(org):
     task_result = dict(time=datetime_to_ms(timezone.now()), counts=dict(fetched=total_runs))
     org.set_task_result(TaskType.fetch_runs, task_result)
 
-    r.set(last_time_key, format_iso8601(until))
+    redis_connection.set(last_time_key, format_iso8601(until))
 
 
 @task
