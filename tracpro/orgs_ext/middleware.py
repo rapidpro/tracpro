37614b75aa71511d3c6882f0a9944680d7c36de4
Erin Mullaney <emullaney@caktusgroup.com>
2015-10-14 13:03:19 -0400

    Move temba exception check into new middleware function.

---
+++ tracpro/orgs_ext/middleware.py
@@ -0,0 +1,15 @@
+from django.utils.translation import ugettext_lazy as _
+from django.http import HttpResponseBadRequest
+
+from temba_client.base import TembaAPIError
+
+
+class HandleTembaAPIError(object):
+
+    def process_exception(self, request, exception):
+        if isinstance(exception, TembaAPIError):
+            return HttpResponseBadRequest(
+                _("Org does not have a valid API Key. " +
+                  "Please edit the org through Site Manage or contact your administrator."))
+
+        pass
