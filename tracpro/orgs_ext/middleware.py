78024c99879796f4ff1959b402b0151b1f74d324
Erin Mullaney <emullaney@caktusgroup.com>
2015-10-16 12:13:24 -0400

    Minor edits to exception handling

--- tracpro/orgs_ext/middleware.py
+++ tracpro/orgs_ext/middleware.py
@@ -1,6 +1,8 @@
 from django.utils.translation import ugettext_lazy as _
 from django.http import HttpResponseBadRequest
 
+from requests import HTTPError
+
 from temba_client.base import TembaAPIError, TembaConnectionError
 
 
@@ -14,19 +16,19 @@ class HandleTembaAPIError(object):
             "Please try again later.")
 
         if isinstance(exception, TembaAPIError):
-            rapidpro_connection_error_codes = ["502", "503", "504"]
+            if isinstance(exception.caused_by, HTTPError):
+                response = exception.caused_by.response
+                if response is not None:
+                    if response.status_code == 403:
+                        return HttpResponseBadRequest(
+                            _("Org does not have a valid API Key. "
+                              "Please edit the org through Site Manage or contact your administrator."))
 
-            if any(code in exception.caused_by.message for code in rapidpro_connection_error_codes):
-                return HttpResponseBadRequest(
-                    rapidProConnectionErrorString)
-
-            else:
-                return HttpResponseBadRequest(
-                    _("Org does not have a valid API Key. "
-                      "Please edit the org through Site Manage or contact your administrator."))
+                    elif response.status_code >= 500:
+                        return HttpResponseBadRequest(rapidProConnectionErrorString)
 
         elif isinstance(exception, TembaConnectionError):
             return HttpResponseBadRequest(
                 rapidProConnectionErrorString)
 
-        pass
+        return None
