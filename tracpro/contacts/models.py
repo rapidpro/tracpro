72eb580d72885735633d47cdbc00d0494ae7f892
Rebecca <rebecca@caktusgroup.com>
2015-11-05 17:38:08 -0500

    Use parse_datetime from Django
    dateutils.parser.parse("1.1") erroneously parses a datetime value.

--- tracpro/contacts/models.py
+++ tracpro/contacts/models.py
@@ -5,12 +5,11 @@ from decimal import Decimal, InvalidOperation
 import logging
 from uuid import uuid4
 
-from dateutil.parser import parse
-
 from django import forms
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.dateparse import parse_datetime
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.text import force_text
 from django.utils.translation import ugettext_lazy as _
@@ -323,13 +322,7 @@ class ContactField(models.Model):
         if self.value is None:
             return None
         elif self.field.value_type == DataField.TYPE_DATETIME:
-            try:
-                return parse(self.value)
-            except ValueError:
-                logger.warning(
-                    "Unable to parse {} value for {} as datetime: {}".format(
-                        self.value, self.contact, self.value))
-                return None
+            return parse_datetime(self.value)
         elif self.field.value_type == DataField.TYPE_NUMERIC:
             try:
                 return Decimal(self.value)
