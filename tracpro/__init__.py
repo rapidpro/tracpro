a92118d7ee6acde57ab9853186c43a5c6748e8a6
Rebecca <rebecca@caktusgroup.com>
2015-11-18 11:16:47 -0500

    Use tuple to represent version

--- tracpro/__init__.py
+++ tracpro/__init__.py
@@ -5,4 +5,23 @@ from __future__ import absolute_import
 from .celery import app as celery_app  # noqa
 
 
-__version__ = "1.0.0"
+VERSION = (1, 0, 0, "dev")
+
+
+def get_version(version):
+    assert len(version) == 4, "Version must be formatted as (major, minor, micro, state)"
+
+    major, minor, micro, state = version
+
+    assert isinstance(major, int), "Major version must be an integer."
+    assert isinstance(minor, int), "Minor version must be an integer."
+    assert isinstance(micro, int), "Micro version must be an integer."
+    assert state in ('final', 'dev'), "State must be either final or dev."
+
+    if state == 'final':
+        return "{}.{}.{}".format(major, minor, micro)
+    else:
+        return "{}.{}.{}.{}".format(major, minor, micro, state)
+
+
+__version__ = get_version(VERSION)
