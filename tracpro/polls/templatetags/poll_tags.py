6c2a8755f9ee3e2919a06aee25a718a916c2ad1a
Rebecca <rebecca@caktusgroup.com>
2016-01-13 15:05:16 -0500

    Reduce magic surrounding form creation

--- tracpro/polls/templatetags/poll_tags.py
+++ tracpro/polls/templatetags/poll_tags.py
@@ -4,23 +4,10 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag("polls/_filters_fieldset.html")
-def fieldset(form, *fields, **kwargs):
-    field_count = len(fields)
-    col_width = 12 / field_count
-    label_width = kwargs.pop('label_width', (2, 4, 6, 12)[field_count])
-    field_width = 12 - label_width
-
-    bound_fields = []
-    for name in fields:
-        field = form[name]
-        field.col_width = col_width
-        field.label_width = label_width
-        field.field_width = field_width
-        bound_fields.append(field)
-
+@register.inclusion_tag("polls/_filters_field.html")
+def field(form, field_name, **kwargs):
     kwargs.update({
         'form': form,
-        'fields': bound_fields,
+        'field': form[field_name],
     })
     return kwargs
