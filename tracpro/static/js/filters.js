ef3d25131cd1500d8c470a53205e5adac3a8f1ec
Rebecca <rebecca@caktusgroup.com>
2016-01-13 17:24:17 -0500

    Factor out useful filters javascript

---
+++ tracpro/static/js/filters.js
@@ -0,0 +1,36 @@
+$(function() {
+    /* Initialize date fields. */
+    $(".filter-form input[class^='datepicker']").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        selectOtherMonths: true,
+        showOtherMonths: true,
+        yearRange: "2013:" + new Date().getFullYear()
+    });
+
+    /* Update button text when filter form display is toggled. */
+    $('#filters').on('show.bs.collapse', function() {
+        $('#toggle-filters').text('Hide filters...');
+    });
+    $('#filters').on('hide.bs.collapse', function() {
+        $('#toggle-filters').text('Show filters...');
+    });
+
+    /* Don't show or submit custom date fields if they are not needed. */
+    $('.filter-form #id_date_range').on('change', function() {
+        var showDates = $(this).val() === 'custom';
+        $('#filter-dates').toggleClass('hidden', !showDates);
+        $('#filter-dates').find('input').prop('disabled', !showDates)
+    }).change();
+
+    /* Don't submit empty form fields. */
+    $('.filter-form').on('submit', function() {
+        $(this).find('input,select').each(function(i, item) {
+          var self = $(this);
+          if (!self.val()) {
+            self.prop('disabled', true);
+          }
+        });
+        return true;
+    });
+});
--- tracpro/static/js/poll-charts.js
+++ tracpro/static/js/poll-charts.js
@@ -137,45 +137,8 @@ $(function() {
         colors: ['#7cb5ec', '#90ed7d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1']
     });
 
-    /* Initialize date fields. */
-    if ($("input[class^='datepicker']").length) {
-        $("input[class^='datepicker']").datepicker({
-            changeMonth: true,
-            changeYear: true,
-            selectOtherMonths: true,
-            showOtherMonths: true,
-            yearRange: "2013:" + new Date().getFullYear()
-        });
-    }
-
-    /* Update button text when filter form display is toggled. */
-    $('#filters').on('show.bs.collapse', function() {
-        $('#toggle-filters').text('Hide filters...');
-    });
-    $('#filters').on('hide.bs.collapse', function() {
-        $('#toggle-filters').text('Show filters...');
-    });
-
-    /* Don't submit custom date fields if they are not used. */
-    $('.filter-form #id_date_range').on('change', function() {
-        var showDates = $(this).val() === 'custom';
-        $('#filter-dates').toggleClass('hidden', !showDates);
-        $('#filter-dates').find('input').prop('disabled', !showDates)
-    }).change();
-
-    /* Don't submit empty form fields. */
-    $('.filter-form').on('submit', function() {
-        $(this).find('input,select').each(function(i, item) {
-          var self = $(this);
-          if (!self.val()) {
-            self.prop('disabled', true);
-          }
-        });
-        return true;
-    });
-
     /* Update numeric data display on the client side. */
-    $('#id_numeric').on('change', function() {
+    $('.filter-form #id_numeric').on('change', function() {
         $('.chart-numeric').chart_numeric();
     });
 
@@ -183,6 +146,5 @@ $(function() {
     $('.chart-open-ended').chart_open_ended();
     $('.chart-numeric').chart_numeric();
     $('.chart-multiple-choice').chart_multiple_choice();
-
     $('.chart-bar').chart_bar();
 });
