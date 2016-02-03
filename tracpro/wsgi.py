09ee6d75924a280e0d9c56a73bae069f0e318b7b
Erin Mullaney <emullaney@caktusgroup.com>
2016-02-03 11:17:54 -0500

    Updating to latest and greatest caktus project settings with new .env set up

--- tracpro/wsgi.py
+++ tracpro/wsgi.py
@@ -1,5 +1,9 @@
 import os
 
+from . import load_env
+
+load_env.load_env()
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracpro.settings")
 
 from django.core.wsgi import get_wsgi_application  # noqa
