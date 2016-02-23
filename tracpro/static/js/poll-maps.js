ae6de986ada7275497f2cfeab47b3bf39801059d
Erin Mullaney <emullaney@caktusgroup.com>
2016-02-23 09:38:06 -0500

    Adding back id to map div, wrapping map js inside of jquery page load, correcting leaflet includes

--- tracpro/static/js/poll-maps.js
+++ tracpro/static/js/poll-maps.js
@@ -2,58 +2,60 @@
 // http://jsfiddle.net/erinw/ukdegfpb/5/
 // http://jsfiddle.net/zafrii/4cu28pae/
 
+$(function() {
+  $.getJSON( "/boundary/", function( data ) {
+    var items = [];
+    $.each( data, function( key, val ) {
+      all_boundaries = val;
+    });
 
-$.getJSON( "/boundary/", function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    all_boundaries = val;
-  });
+    $('.map').each(function() {
+      var map_div = $(this);
+      var map_data = map_div.data('maps');
+      var colors = map_div.data('colors');
 
-  $('.map').each(function() {
-    var map_div = $(this);
-    var map_data = map_div.data('maps');
-    var colors = map_div.data('colors');
+      var map = L.map(this.id);
 
-    var map = L.map(this.id);
-
-    boundaries_array = [];
-    for (data_index in map_data) {
-      for (b_index in all_boundaries) {
-        if (all_boundaries[b_index].properties.id == map_data[data_index].boundary) {
-          all_boundaries[b_index].properties.style.fillColor = map_data[data_index].color;
-          mp = all_boundaries[b_index];
-          boundaries = new L.GeoJSON(mp, {
-            style: function(feature) {
-              return feature.properties.style
-            }
-          });
-          boundaries.addTo(map);
-          boundaries_array.push(boundaries);
+      boundaries_array = [];
+      for (data_index in map_data) {
+        for (b_index in all_boundaries) {
+          if (all_boundaries[b_index].properties.id == map_data[data_index].boundary) {
+            all_boundaries[b_index].properties.style.fillColor = map_data[data_index].color;
+            mp = all_boundaries[b_index];
+            boundaries = new L.GeoJSON(mp, {
+              style: function(feature) {
+                return feature.properties.style
+              }
+            });
+            console.log(boundaries);
+            boundaries.addTo(map);
+            boundaries_array.push(boundaries);
+          }
         }
       }
-    }
 
-    // Center the map to include all boundaries
-    var boundaries_group = new L.featureGroup(boundaries_array);
-    map.fitBounds(boundaries_group.getBounds());
+      // Center the map to include all boundaries
+      var boundaries_group = new L.featureGroup(boundaries_array);
+      map.fitBounds(boundaries_group.getBounds());
 
-    var legend = L.control({
-        position: 'bottomright'
+      var legend = L.control({
+          position: 'bottomright'
+      });
+      legend.onAdd = function (map) {
+
+          var colors = map_div.data('colors')
+          var div = L.DomUtil.create('div', 'info legend');
+          var label = ['<strong>index</strong>'];
+          for (key in colors) {
+            div.innerHTML += label.push(
+              '<div class="legend_color" style="background:' + colors[key] + '"></div><span>' + key + '</span>');
+          }
+
+          div.innerHTML = label.join('<br>');
+          return div;
+      };
+
+      legend.addTo(map);
     });
-    legend.onAdd = function (map) {
-
-        var colors = map_div.data('colors')
-        var div = L.DomUtil.create('div', 'info legend');
-        var label = ['<strong>index</strong>'];
-        for (key in colors) {
-          div.innerHTML += label.push(
-            '<div class="legend_color" style="background:' + colors[key] + '"></div><span>' + key + '</span>');
-        }
-
-        div.innerHTML = label.join('<br>');
-        return div;
-    };
-
-    legend.addTo(map);
   });
 });
