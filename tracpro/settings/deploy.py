fc9b913d088e130b0044feaef89f91bcc55a73f4
Rebecca <rebecca@caktusgroup.com>
2016-03-09 15:10:16 -0500

    Use env utilities for existing deploy settings

--- tracpro/settings/deploy.py
+++ tracpro/settings/deploy.py
@@ -3,16 +3,17 @@ from __future__ import unicode_literals
 from .base import *  # noqa
 
 
-WEBSERVER_ROOT = '/var/www/tracpro/'
+require_env('DOMAIN', 'ENVIRONMENT')
 
-ENVIRONMENT = os.environ.get('ENVIRONMENT', '').lower()
+ENVIRONMENT = from_env('ENVIRONMENT').lower()
+
+WEBSERVER_ROOT = '/var/www/tracpro/'
 
 ADMINS = [
     ('Caktus EduTrac Team', 'edutrac-team@caktusgroup.com'),
 ]
 
-ALLOWED_HOSTS = ['.' + os.environ['DOMAIN']]
-
+ALLOWED_HOSTS = ['.' + from_env('DOMAIN')]
 
 CACHES['default']['LOCATION'] = 'localhost:6379:4'
 
@@ -29,38 +30,38 @@ COMPRESS_OFFLINE_CONTEXT = {
     'testing': False,
 }
 
-CSRF_COOKIE_DOMAIN = os.environ.get('CSRF_COOKIE_DOMAIN')
+CSRF_COOKIE_DOMAIN = from_env_or_django('CSRF_COOKIE_DOMAIN')
 
 DATABASES['default'].update({
     'NAME': 'tracpro_{}'.format(ENVIRONMENT),
     'USER': 'tracpro_{}'.format(ENVIRONMENT),
-    'HOST': os.environ.get('DB_HOST', ''),
-    'PORT': os.environ.get('DB_PORT', ''),
-    'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+    'HOST': from_env('DB_HOST', ''),
+    'PORT': from_env('DB_PORT', ''),
+    'PASSWORD': from_env('DB_PASSWORD', ''),
 })
 
 DEFAULT_FROM_EMAIL = 'no-reply@caktusgroup.com'
 
 EMAIL_SUBJECT_PREFIX = '[Edutrac {}]'.format(ENVIRONMENT.title())
 
-HOSTNAME = os.environ['DOMAIN']
+HOSTNAME = from_env('DOMAIN')
 
 LOGGING['handlers']['file']['filename'] = os.path.join(WEBSERVER_ROOT, 'log', 'tracpro.log')
 
 MEDIA_ROOT = os.path.join(WEBSERVER_ROOT, 'public', 'media')
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = from_env('SECRET_KEY')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'HTTPS')
 
 SESSION_CACHE_ALIAS = "default"
 
-SESSION_COOKIE_DOMAIN = os.environ.get('SESSION_COOKIE_DOMAIN')
+SESSION_COOKIE_DOMAIN = from_env('SESSION_COOKIE_DOMAIN')
 
 SESSION_COOKIE_SECURE = False
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
-SITE_HOST_PATTERN = os.environ.get('SITE_HOST_PATTERN')
+SITE_HOST_PATTERN = from_env('SITE_HOST_PATTERN')
 
 STATIC_ROOT = os.path.join(WEBSERVER_ROOT, 'public', 'static')
