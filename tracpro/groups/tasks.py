e63634a4b30226f51e302642e459949393b882a1
Rebecca <rebecca@caktusgroup.com>
2016-03-28 13:36:02 -0400

    Class-based task need not use @task decorator

--- tracpro/groups/tasks.py
+++ tracpro/groups/tasks.py
@@ -1,13 +1,10 @@
 from __future__ import unicode_literals
 
-from djcelery_transactions import task
-
 from django.apps import apps
 
 from tracpro.orgs_ext.tasks import OrgTask
 
 
-@task
 class SyncOrgBoundaries(OrgTask):
 
     def org_task(self, org):
