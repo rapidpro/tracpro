d03163c66b68459de74e26d02c2e0df984ffc1d2
David Ray <dray@caktusgroup.com>
2016-05-02 14:22:07 -0400

    allow for total configuration of the path

--- tracpro/settings/deploy.py
+++ tracpro/settings/deploy.py
@@ -73,7 +73,7 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 STATIC_ROOT = os.path.join(WEBSERVER_ROOT, 'public', 'static')
 
 if from_env('CUSTOM_STATICFILES_DIR'):
-    STATICFILES_DIRS.insert(0, from_env('CUSTOM_STATICFILES'))
+    STATICFILES_DIRS.insert(0, from_env('CUSTOM_STATICFILES_DIR'))
 
 if from_env('CUSTOM_TEMPLATE_DIR'):
     TEMPLATE_DIRS.insert(0, from_env('CUSTOM_TEMPLATE_DIR'))
