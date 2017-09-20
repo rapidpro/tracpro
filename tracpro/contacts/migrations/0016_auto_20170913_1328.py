ed9b671a3902811487b54c51376e45ba95aad317
philtuskha <phil.tuskha@gmail.com>
2017-09-20 16:43:23 -0400

    added backwards migration

--- tracpro/contacts/migrations/0016_auto_20170913_1328.py
+++ tracpro/contacts/migrations/0016_auto_20170913_1328.py
@@ -4,10 +4,27 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 
 def forward(apps, schema_editor):
+    ''' Copies any existing group this contact may have into groups'''
     Contact = apps.get_model("contacts", "Contact")
+    for contact in Contact.objects.exclude(group=None):
+        contact.groups.add(contact.group)
+
+def backward(apps, schema_editor):
+    ''' Takes the first group of contact's several groups
+        and adds to the newly reproduced field 'group'.
+        For development purposes, this seems resonable. '''
+    Contact = apps.get_model("contacts", "Contact")
+    migrations.AddField(
+            model_name='contact',
+            name='group',
+            field=models.ForeignKey(related_name='old_contacts', verbose_name='group', to='groups.Group')
+        )
     for contact in Contact.objects.all():
-        if contact.group:
-            contact.groups.add(contact.group)
+        if contact.groups.first():
+            this_contact = Contact.objects.get(id=contact.id)
+            this_contact.group_id = contact.groups.first().pk
+            this_contact.save()
+
 
 class Migration(migrations.Migration):
 
@@ -16,5 +33,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(forward)
+        migrations.RunPython(forward, backward)
     ]
