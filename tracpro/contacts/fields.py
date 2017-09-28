35567a6280b5d7bd28f092b432519d116e309fdc
Dan Poirier <dpoirier@caktusgroup.com>
2017-09-28 13:35:16 -0400

    WIP

--- tracpro/contacts/fields.py
+++ tracpro/contacts/fields.py
@@ -1,6 +1,10 @@
 from __future__ import unicode_literals
 
+import re
+
+import phonenumbers
 from django import forms
+from django.core.exceptions import ValidationError
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
@@ -41,6 +45,36 @@ class URNWidget(forms.widgets.MultiWidget):
         return mark_safe(''.join(output))
 
 
+def validate_twitter_handle(handle):
+    handle_regex = r'^[a-zA-Z0-9_]{1,15}$'
+    if not re.match(handle_regex, handle):
+        raise ValidationError(
+            _("That is not a valid format Twitter handle.  A valid handle has only "
+              "letters A-Z, numbers 0-9, and underscores (_), and has up to 15 "
+              "characters."))
+
+
+def validate_twitter_id(twitter_id):
+    if not twitter_id.is_digit():  # True only if all chars are digits
+        raise ValidationError(
+            _("That is not a valid format Twitter ID.  A valid ID has only "
+              "numbers 0-9."))
+
+
+def validate_phone(number):
+    if number[0] != '+':
+        raise ValidationError(
+            _("Phone numbers must start with + and the country code, "
+              "e.g. +19995551212."))
+    # Validate the number the same way that RapidPro will?
+    try:
+        parsed = phonenumbers.parse(path, country_code)
+        return phonenumbers.is_possible_number(parsed)
+    except Exception:
+        return False
+
+
+
 class URNField(forms.fields.MultiValueField):
 
     def __init__(self, *args, **kwargs):
@@ -58,3 +92,18 @@ class URNField(forms.fields.MultiValueField):
 
     def compress(self, values):
         return '%s:%s' % (values[0], values[1])
+
+    def clean(self, value):
+        compressed_value = super(URNField, self).clean(value)
+        scheme, address = compressed_value.split(':', 1)
+        if not address:
+            raise ValidationError(_("URN is required"))
+        if scheme == URN_SCHEME_TWITTER:
+            validate_twitter_handle(address)
+        elif scheme == URN_SCHEME_TWITTERID:
+            if '#' in address:
+                twitter_id, handle = address.split('#', 1)
+                validate_twitter_handle(handle)
+                validate_twitter_id(twitter_id)
+            else:
+                validate_twitter_id(address)
